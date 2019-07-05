44b8f4908167aabb32e105ec5977f78c38af83e3
Alexander Rudyk <alakai.1998.28@gmail.com>
2019-07-05 11:49:43 +0300

    Add Unit test

--- Genesis_TestTests/Genesis_TestTests.swift
+++ Genesis_TestTests/Genesis_TestTests.swift
@@ -20,15 +20,10 @@ class Genesis_TestTests: XCTestCase {
     }
 
     func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        XCTAssertTrue(true)
     }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    func testFail() {
+        XCTAssertTrue(false, "Fail test")
     }
-
 }
